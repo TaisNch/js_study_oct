aa7417a1b9b29f9e73310f9c2836fc68859e6246
TaisNch <tais.aliexpress@gmail.com>
2024-09-30 10:13:39 +0200

    lesson02

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const title =0;
-const screens=0;
-const screenPrice=0;
-const rollback=0;
-const fullPrice=0;
-const adaptive=0;
+let title = "Название проекта";  
+let screens = "Простые, Сложные, Интерактивные";  
+let screenPrice = 5000; 
+let rollback = 25;  
+let fullPrice = 100000; 
+let adaptive = true; 
 
-alert("Hello");
-console.log ('This is lesson 01');
+// Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+// Вывести в консоль длину строки из переменной screens
+console.log (screens.length);
+// Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
+console.log (`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log (`Стоимость разработки сайта ${fullPrice} рублей`);
+// Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
+console.log(screens.toLowerCase());
+console.log(screens.split(','));
+// Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
+console.log(fullPrice * (rollback/100));
