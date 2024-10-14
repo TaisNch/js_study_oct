96843f77d73f845d78143a4fc8a40b88e2d198d1
TaisNch <tais.aliexpress@gmail.com>
2024-10-14 18:48:32 +0200

    lesson07

--- script.js
+++ script.js
@@ -1,86 +1,87 @@
-
-const title = prompt ('Какое название проекта хотите?');
-const screens = prompt ('Какие типы экранов нужно разработать? - Простые, Сложные, Интерактивные')
-const adaptive = prompt ('Нужен ли адаптив на сайте?', 'Да');
-const service1 = prompt ('Какой дополнительный тип услуги нужен?');
-const servicePrice1 = +prompt (`Сколько это будет стоить ${service1}?`);
-const service2 = prompt ('Какой дополнительный тип услуги нужен?');
-const servicePrice2 = +prompt (`Сколько это будет стоить ${service2}?`);
-const rollback = 25;
-let screenPrice
-// const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-// const servicePercentPrice = fullPrice - fullPrice * (rollback/100); 
-
-let screenPriceFnc = function() {
-  do {
-    screenPrice = prompt("Введите стоимость");
-} while (isNaN(screenPrice) || screenPrice <= 0);
-
-};
-
-const showTypeOf = function(item)  {
-return console.log (item, typeof item)
-}
-
-const allServicePrices = function getAllServicePrices() {
-return servicePrice1 + servicePrice2
-  }
-console.log (`Стоимость всех дополнительных услуг составит ${allServicePrices()}`)
-const fullPrice = function getFullPrice() {
-  return screenPriceFnc() + allServicePrices()
-  }
-console.log (`Стоимость всех дополнительных услуг составит ${fullPrice()}`)
-
-
-if (fullPrice>=3000) {
+const array = {
+  title : '',
+  screens: '',
+  adaptive: true,
+  service1: '',
+  service2: '',
+  screenPrice: 0,
+  servicePrice1:0,
+  servicePrice2:0,
+  rollback:25,
+  start: function () {
+    array.title = prompt ('Как называется Ваш проект', 'Калькулятор')
+    array.screens = prompt ('Какие типы экранов нужно разработать?', "Простые, Сложные")
+    do {
+    array.screenPrice = +prompt("Введите стоимость");
+  } while (isNaN(array.screenPrice) || array.screenPrice <= 0);
+    array.adaptive = confirm('Нужен ли адаптив на странице?');
+    array.service1 = prompt ('Какой дополнительный тип услуги нужен?');
+    array.servicePrice1 = +prompt (`Сколько это будет стоить ${array.service1}?`);
+    array.service2 = prompt ('Какой дополнительный тип услуги нужен?');
+    array.servicePrice2 = +prompt (`Сколько это будет стоить ${array.service2}?`);
+  },
+  allServicePrices: function() {
+    return array.servicePrice1 + array.servicePrice2 },
+  fullPrice: function() {
+    return array.screenPrice +array.allServicePrices() },
+  servicePercentPrice: function() {
+    return array.fullPrice() - array.fullPrice() * (array.rollback/100)
+    },
+  discount: function() {
+    if (array.fullPrice()>=3000) {
   console.log ('Мы можем дать скидку - 10%');
- } else if (fullPrice<30000 && fullPrice>=15000) {
+    } else if (array.fullPrice()<30000 && array.fullPrice()>=15000) {
    console.log ('Мы можем дать скидку - 5%');
- }
- else if (fullPrice<15000 && fullPrice>0)
-   console.log ('Скидка не предусмотрена');
- else {
+    }
+    else if (array.fullPrice()<15000 && array.fullPrice()>0)
+       console.log ('Скидка не предусмотрена');
+     else {
    console.log ('Что-то пошло не так, надо уточнить стоимость проекта и откат посреднику'); 
  }
+  },
+ getUpperTitle: function () {
+      const trimmedTitle = array.title.trim(); 
+      const firstLetter = array.trimmedTitle.substring(0, 1).toUpperCase(); 
+      const remainingLetters = array.trimmedTitle.substring(1).toLowerCase(); 
+      const newTitle = `${firstLetter}${remainingLetters}`; 
+      return newTitle;
+    },
+  logger:function() {
+    console.log(`Полная стоимость проекта: ${array.fullPrice()}`);
+    console.log(`Cтоимость проекта с учетом отката: ${array. servicePercentPrice()}`);
+  }
+  }
+   
+  array.start();
+  array.logger();
+  
+
  
-const getUpperTitle = function getTitle() {
-  const trimmedTitle = title.trim(); 
-  const firstLetter = trimmedTitle.substring(0, 1).toUpperCase(); 
-  const remainingLetters = trimmedTitle.substring(1).toLowerCase(); 
-  const newTitle = `${firstLetter}${remainingLetters}`; 
-  return newTitle;
-}
 
-console.log(`Название проекта: ${getUpperTitle()}`);
 
-const servicePercentPrice = function getServicePercentPrices() {
-  return fullPrice() - fullPrice() * (rollback/100); 
-}
-console.log(`Окончательная цена с учетом отката посреднику - ${servicePercentPrice()}`);
 
-const getRollbackMessage = function RollbackMessage (price) {
-  if (price>=3000) {
-    return 'Мы можем дать скидку - 10%';
-   } else if (price<30000 && price>=15000) {
-    return'Мы можем дать скидку - 5%';
-   }
-   else if (price<15000 && price>0)
-    return 'Скидка не предусмотрена';
-   else {
-    return 'Что-то пошло не так, надо уточнить стоимость проекта и откат посреднику'; 
-   }
-}
-console.log(`Скидка, которую можем предложить - ${getRollbackMessage(fullPrice())}`)
+// const getUpperTitle = function getTitle() {
+//   const trimmedTitle = array.title.trim(); 
+//   const firstLetter = array.trimmedTitle.substring(0, 1).toUpperCase(); 
+//   const remainingLetters = array.trimmedTitle.substring(1).toLowerCase(); 
+//   const newTitle = `${firstLetter}${remainingLetters}`; 
+//   return newTitle;
+// }
 
-console.log (`нужна ${service1}`);
-console.log (`стоимость ${service1} составит ${servicePrice1}`);
-console.log (`нужна ${service2}`);
-console.log (`стоимость ${service2} составит ${servicePrice2}`);
-console.log(`Окончательная цена - ${fullPrice()}`);
-console.log(`Окончательная цена с учетом отката посреднику - ${servicePercentPrice()}`);
+// console.log(`Название проекта: ${getUpperTitle()}`);
 
-showTypeOf(screenPrice);
-showTypeOf(screens);
-showTypeOf(title);
-showTypeOf(adaptive);
+// const getRollbackMessage = function RollbackMessage (price) {
+//   if (price>=3000) {
+//     return 'Мы можем дать скидку - 10%';
+//    } else if (price<30000 && price>=15000) {
+//     return'Мы можем дать скидку - 5%';
+//    }
+//    else if (price<15000 && price>0)
+//     return 'Скидка не предусмотрена';
+//    else {
+//     return 'Что-то пошло не так, надо уточнить стоимость проекта и откат посреднику'; 
+//    }
+// }
+
+
 
